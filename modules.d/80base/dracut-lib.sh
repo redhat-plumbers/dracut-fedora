a460137bb25a5fdc69fbad1014cc94fee76cbc53
Pavel Valena <pvalena@redhat.com>
2026-05-07 00:59:05 +0200

    fix(base): replace eval with safe variable indirection in splitsep and export_n
    splitsep: use local nameref to avoid eval injection via single-quote breakout.
    export_n: use ${!var} and printf -v to avoid eval injection via double-quote breakout.
    
    (cherry picked from commit 2a395c7d4abe8e26836ecf109ab69600d4243e03)

--- modules.d/80base/dracut-lib.sh
+++ modules.d/80base/dracut-lib.sh
@@ -336,12 +336,18 @@ splitsep() {
 
     while [ -n "$str" ] && [ "$#" -gt 1 ]; do
         tmp="${str%%"$sep"*}"
-        eval "$1='${tmp}'"
+        local -n _splitsep_ref="$1"
+        _splitsep_ref="$tmp"
+        unset -n _splitsep_ref
         str="${str#"$tmp"}"
         str="${str#"$sep"}"
         shift
     done
-    [ -n "$str" ] && [ -n "$1" ] && eval "$1='$str'"
+    if [ -n "$str" -a -n "$1" ]; then
+        local -n _splitsep_ref="$1"
+        _splitsep_ref="$str"
+        unset -n _splitsep_ref
+    fi
     debug_on
     return 0
 }
@@ -940,14 +946,13 @@ emergency_shell() {
 }
 
 # Retain the values of these variables but ensure that they are unexported
-# This is a POSIX-compliant equivalent of bash's "export -n"
 export_n() {
     local var
     local val
     for var in "$@"; do
-        eval "val=\$$var"
+        val="${!var}"
         unset "$var"
-        [ -n "$val" ] && eval "$var=\"$val\""
+        [ -n "$val" ] && printf -v "$var" '%s' "$val"
     done
 }
 
